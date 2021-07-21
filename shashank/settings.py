266e966743fd081dc68aab9aa4f1c5e0bc65fe8b
ShashankZobb <shashanknke1112@gmail.com>
2021-07-21 21:02:40 +0530

    Update settings.py

--- shashank/settings.py
+++ shashank/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i9mr$_9-*5dgoq_b1k)$qj2o_62s6-_k#=%5oqxgz!x#4kp!q8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
